9c7276c10e6983945c1a53aa431bb0380c1d5582
Andrew Branch <andrewbranch@mail.com>
2015-07-18 11:03:58 -0700

    Add HTTP client dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Bacon.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [
+      :logger,
+      :httpotion
+    ]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +30,9 @@ defmodule Bacon.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.1"},
+      {:httpotion, "~> 2.1.0"}
+    ]
   end
 end
