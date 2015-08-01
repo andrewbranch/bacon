05bc27a3bdf236574bab3fb7959e4dd472a57f2f
Andrew Branch <andrewbranch@mail.com>
2015-08-01 14:30:58 -0700

    Make a better async API

--- lib/wiki_client.ex
+++ lib/wiki_client.ex
@@ -1,6 +1,16 @@
 defmodule Bacon.WikiClient do
+  alias HTTPotion.AsyncChunk
+  alias HTTPotion.AsyncEnd
   use HTTPotion.Base
   require Logger
+  
+  def get_backlinks(article, pid) do
+    aggregator = spawn aggregate_response(pid)
+    %{id: id} = get article, [stream_to: aggregator]
+    # Logger.debug "sending request id #{id |> to_string} to aggregator..."
+    send aggregator, {:start, id}
+    id
+  end
   
   def process_url(article) do
     "https://en.wikipedia.org/w/api.php?action=query&list=backlinks&format=json&bllimit=500&bltitle=" <> article
@@ -12,15 +22,27 @@ defmodule Bacon.WikiClient do
   
   def process_response_body(body) do
     (
-      {:ok, data} = body
+      {:ok, _data} = body
       |> to_string
       |> Poison.Parser.parse
     )
     |> elem 1
   end
   
-  # I can’t find a scenario when the chunk isn’t the whole body
-  def process_response_chunk(chunk) do
-    process_response_body chunk
+  defp extract_backlinks(body) do
+    process_response_body(body)["query"]["backlinks"]
+  end
+  
+  defp aggregate_response(pid, request_id \\ nil, chunks \\ nil) do
+    fn ->
+      receive do
+        {:start, id} ->
+          aggregate_response(pid, id, "").()
+        %AsyncChunk{id: ^request_id, chunk: chunk} ->
+          aggregate_response(pid, request_id, chunks <> chunk).()
+        %AsyncEnd{id: ^request_id} ->
+          send pid, %{id: request_id, backlinks: extract_backlinks(chunks)}
+      end
+    end
   end
 end
--- test/wiki_client_test.exs
+++ test/wiki_client_test.exs
@@ -3,23 +3,22 @@ defmodule Bacon.WikiClientTest do
   alias Bacon.WikiClient
 
   test "response body is a map for valid article name" do
-    response = WikiClient.get("Kevin_Bacon")
+    response = WikiClient.get("List_of_fictional_big_cats")
     assert response.body |> is_map
   end
   
   test "can make multiple concurrent requests" do
-    backlinks = ["Apollo 13", "Jello Biafra", "List of fictional dogs"]
-    ["Kevin_Bacon", "Portlandia_(TV_series)", "List_of_fictional_big_cats"]
+    expected = ["Jello Biafra", "List of fictional dogs"]
+    ["Portlandia_(TV_series)", "List_of_fictional_big_cats"]
     |> Enum.map(fn (article) ->
-      WikiClient.get article, [stream_to: self]
+      WikiClient.get_backlinks article, self
     end)
     |> Enum.with_index
     |> Enum.each fn (tuple) ->
-      {%{id: id}, index} = tuple
-      assert_receive %HTTPotion.AsyncChunk{id: ^id, chunk: body}, 5000
-      assert_receive %HTTPotion.AsyncEnd{id: ^id}, 5000
-      assert body["query"]["backlinks"] |> Enum.any? fn (link) ->
-        link["title"] == backlinks |> Enum.at index
+      {id, index} = tuple
+      assert_receive %{id: ^id, backlinks: backlinks}, 5000
+      assert backlinks |> Enum.any? fn (link) ->
+        link["title"] == expected |> Enum.at index
       end
     end
   end
